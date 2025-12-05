bfd02067646e6974b822ba2cafb8e5eea63c660d
Maya321-wq <s-maya.akl@zewailcity.edu.eg>
2025-12-05 19:48:59 +0200

    chore: setup ESLint + Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ["react", "jsx-a11y", "react-hooks"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off"
+  },
+  settings: { react: { version: "detect" } }
+};
